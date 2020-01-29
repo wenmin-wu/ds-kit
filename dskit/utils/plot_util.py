f2dfe953317c9deb99b50efaa2929c751c4c18d6
William Wu <william.wu@grabtaxi.com>
2020-01-29 10:11:19 +0000

    sperate dfs names

--- dskit/utils/plot_util.py
+++ dskit/utils/plot_util.py
@@ -1,24 +1,30 @@
 from matplotlib import pyplot as plt
+import numpy as np
 
 def get_min_max(df, col):
     assert col in df.columns, f'{col} not found in dataframe columns'
     return df[col].values.min(), df[col].values.max()
 
-def plot_fea_hists(dfs, col, ax, y_scale=None):
+def plot_fea_hists(dfs, names, col, ax):
+    if names is not None:
+        assert len(dfs) == len(names), 'len of DataFrames not euqal to len of names'
     min_val, max_val = np.inf, -np.inf
-    for (name, df) in dfs:
+    for df in dfs:
         curr_min_val, curr_max_val = get_min_max(df, col)
         min_val = min(min_val, curr_min_val)
         max_val = max(max_val, curr_max_val)
-    for (name, df) in dfs:
+    for i, df in enumerate(dfs):
+        name = names[i] if names is not None else None
         df[col].hist(ax=ax, bins=np.linspace(min_val, max_val, 20), label=name)
-    ax.legend()
+    if names is not None:
+        ax.legend()
     ax.set_title(f'{col}')
-    if y_scale is not None:
-        ax.set_yscale(y_scale)
-    
-    
-def plot_df_feas_hists(dfs, cols, num_col=4, y_scale=None):
+    ax.set_yscale('log')
+    if max_val > 100:
+        ax.set_xscale('log')
+
+
+def plot_df_feas_hists(*dfs, names=None, cols=None, num_col=4):
     """
     list of name and df pairs
     """
@@ -31,5 +37,5 @@ def plot_df_feas_hists(dfs, cols, num_col=4, y_scale=None):
     axes = axes.flatten()
     fig.set_size_inches(20, round(ax_height * num_row))
     for i, col in enumerate(cols):
-        plot_fea_hists(dfs, cols[i], axes[i], y_scale)
+        plot_fea_hists(dfs, names, cols[i], axes[i])
     plt.show()
