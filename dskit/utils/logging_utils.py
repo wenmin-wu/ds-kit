fbb684203815cc87ef8ee41582c4cb08de53331a
wenmin-wu <wuwenmin1991@gmail.com>
2022-12-17 05:04:35 +0700

    updating logging_utils.py

--- dskit/utils/logging_utils.py
+++ dskit/utils/logging_utils.py
@@ -1,40 +1,23 @@
 import logging
-from logging.handlers import RotatingFileHandler
+import colorlog
 
-DEFAULT_LOG_FMT = "\x1b[1m\x1b[33m[%(levelname)s %(asctime)s.%(msecs)03d %(name)s]\x1b[0m: %(message)s"
-DEFAULT_LOG_DATE_FMT = "%Y-%m-%d %H:%M:%S"
+_DEFAULT_LOG_FMT = "%(log_color)s[%(levelname)s %(asctime)s.%(msecs)03d %(name)s]:%(reset)s %(message)s"
+_DEFAULT_LOG_DATE_FMT = "%Y-%m-%d %H:%M:%S"
 
 
 def get_logger(
-    logger_name,
-    file_name=None,
-    fmt=None,
-    dt_fmt=None,
-    console_out=True,
-    max_bytes=2000,
-    backup_count=5,
-):
-
-    assert console_out or (file_name is not None), "no handler will be added"
-
+    name: str, fmt: str = None, dt_fmt: str = None
+) -> logging.Logger:
     if fmt is None:
-        fmt = DEFAULT_LOG_FMT
+        fmt = _DEFAULT_LOG_FMT
     if dt_fmt is None:
-        dt_fmt = DEFAULT_LOG_DATE_FMT
-    logger = logging.getLogger(logger_name)
+        dt_fmt = _DEFAULT_LOG_DATE_FMT
+
+    handler = colorlog.StreamHandler()
+    handler.setFormatter(colorlog.ColoredFormatter(fmt, dt_fmt))
+    logger = colorlog.getLogger(name)
+    logger.addHandler(handler)
     logger.setLevel(logging.INFO)
-    logger_fmt = logging.Formatter(fmt, dt_fmt)
-
-    if file_name is not None:
-        fh = RotatingFileHandler(
-            file_name, maxBytes=max_bytes, backupCount=backup_count
-        )
-        fh.setFormatter(logger_fmt)
-        logger.addHandler(fh)
-
-    if console_out:
-        ch = logging.StreamHandler()
-        ch.setFormatter(logger_fmt)
-        logger.addHandler(ch)
-
+    logger.propagate = False
     return logger
+
